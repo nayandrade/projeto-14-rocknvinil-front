468ffa4e3276013b7c608ba22111fcd573374cc7
beatriz.nnds <beatriz.nnds@gmail.com>
2022-07-07 09:50:07 -0300

    component style

---
+++ src/components/SupplierProduct.js
@@ -0,0 +1,45 @@
+export default function SupplierProduct ({ albumName, albumYear, albumPic, bandName, prize, discount, date, amountAvailable}) {
+    return (
+        <Container>
+            <AlbumPic>
+                <img src={albumPic} />
+            </AlbumPic>
+            <AlbumInfo>
+                <h4>{bandName} | {albumYear} | {albumName}</h4>
+                <h3>Você o colocou a venda por: {prize} em {date}</h3>
+                <h3>O desconto aplicado foi: {discount}</h3>
+                <h3>A quantidade disponibilizada foi: {amountAvailable}</h3>                
+            </AlbumInfo>
+        </Container>
+    )
+}
+
+const Container=styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px;
+    width: 80vw;
+    height: 80px;
+    border: 2px solid #f2f2f2;
+    border-radius: 10px;
+`
+
+const AlbumPic=styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+`
+
+const AlbumInfo=styled.div`
+    display: flex;
+    flex-direction: column;
+
+    h4 {
+        font-size: 15px;
+    }
+
+    h3 {
+        font-size: 12px;
+    }
+`
--- src/components/SupplierProducts.js
+++ src/components/SupplierProducts.js
@@ -3,7 +3,9 @@ import { useEffect, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import SupplierContext from '../contexts/SupplierContext';
-import Header from './Header'
+import Header from './Header';
+import SupplierProduct from './SupplierProduct';
+import logo from '../logo/logo'
 
 export default function SupplierProducts () {
     const { supplier } = useContext(SupplierContext);
@@ -11,7 +13,7 @@ export default function SupplierProducts () {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const promise = axios.get('https://projeto-14-rocknvinil-back.herokuapp.com/products', {headers: {Authorization: `Bearer ${user.token}`}});
+        const promise = axios.get('https://projeto-14-rocknvinil-back.herokuapp.com/products', {headers: {Authorization: `Bearer ${supplier.token}`}});
         promise.then((res) => {
             setProducts(res.data)
         });
@@ -22,19 +24,105 @@ export default function SupplierProducts () {
 
     return (
         <Container>
-            <Header />
-            <h2>Oi, { supplier.name } </h2>
-            <h3>Confira aqui seus vinis à venda!</h3>
+            <Header>
+                <div>
+                    <h2 onClick={() => navigate('/login')}>Entrar</h2>
+                    <h2 onClick={() => navigate('/cart')}>Carrinho</h2>
+                </div>
+                <div>
+                    <img src={logo} />
+                </div>
+                <div>
+                    <h2>Buscar</h2>
+                    <h2 onClick={() => navigate('/')}>Vinis</h2>
+                    <h2 onClick={() => navigate('/myproducts')}>Meus produtos</h2>
+                </div>             
+            </Header>
+            <SupplierHeader>
+                <h2>Oi, { supplier.name } </h2>
+                <h3>Obrigada por contribuir com nossa loja!</h3>
+                <button onClick={() => navigate('/addproduct')}>+</button>
+            </SupplierHeader>           
             <ProductsForSale>
             <div>
                 {
                     products.length > 0 ? (
                     products.map((product, index) => <SupplierProduct key={index} albumName={product.albumName} albumYear={product.albumName} albumPic={product.albumPic} bandName={product.bandName} prize={product.prize} discount={product.discount} amountAvailable={product.amountAvailable} date={product.registryDay}/> ))
                     :
-                    <ZeroProducts>Você ainda não possui um produto a venda. Cadastre um aqui!</ZeroProducts>
+                    <h4>Você ainda não possui um produto a venda. Cadastre um aqui!</h4>
                 }
             </div>                
             </ProductsForSale>
         </Container>
     )
 }
+
+const Container=styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+
+const Header=styled.div`
+    width: 100vw;
+    height: 200px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    h2 {
+        font-size: 25px;
+        color: #000000;
+    }
+
+    h2:hover {
+        cursor: pointer;
+    }
+
+    img {
+        width: 60px;
+        height: 100px;
+    }
+`
+
+const SupplierHeader=styled.div`
+    width: 100vw;
+    height: 200px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #ffffff;
+    background-color: #000000;
+    opacity: 0.7;
+    
+    h2 {
+        font-size: 25px;
+    }
+
+    h3 {
+        font-size: 20px;
+    }
+
+    button {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;        
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+`
+
+const ProductsForSale=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 30px;
+    border-radius: 10px;
+    color: #ffffff;
+    background-color: #000000;
+    opacity: 0.7;
+    overflow: scroll;
+`
